787cd1c5cbbe84484ed08a230e0f35e111ee08a2
Brandon <brandon.r.young@outlook.com>
2022-08-23 19:25:37 -0400

    finish footer and start body

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import "./styles/globalStyles.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Body />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ghLogo from "../assets/GitHubLogo.png";
+import "../styles/Footer.css";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="footTxt">Made By: Brandon Young 2022</div>
+                <a href="https://github.com/BabySparta/cv-application" className="ghLogo">
+                    <img src={ghLogo} alt="git hub" className="ghImg" />
+                </a>
+            </div>
+        )
+    }
+}
+
+export default Footer;
