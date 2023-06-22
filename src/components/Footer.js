c286d70512c39ebbcd975e7cfa9aa707126fd8fa
BabySparta <123456789+odin@users.noreply.github.com>
2023-06-22 14:51:56 -0400

    Finish CSS for personal inputs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="footer">
-                <div className="footTxt">Made By: Brandon Young 2022</div>
+                <div className="footTxt">Made By: Brandon Young 2023</div>
                 <a href="https://github.com/BabySparta/cv-application" className="ghLogo">
                     <img src={ghLogo} alt="git hub" className="ghImg" />
                 </a>
--- src/components/bodyInputs/personalInput.js
+++ src/components/bodyInputs/personalInput.js
@@ -4,15 +4,19 @@ import "../../styles/inputStyles/inputPersonal.css";
 class InputPersonal extends React.Component {
     render() {
         return (
-            <div className="inputs">
-                <input type="text" className="input" placeholder="First Name"/>
-                <input type="text" className="input" placeholder="Last Name"/>
-                <input type="text" className="input" placeholder="Title"/>
-                <input type="file" className="input fileInput" accept="img/*" title=" "/>
-                <input type="text" className="input" placeholder="Phone Number"/>
-                <input type="text" className="input" placeholder="Email Adress"/>
-                <input type="text" className="input" placeholder="Adress"/>
-                <input type="text" className="input" placeholder="About"/>
+            <div className="personalWrap">
+                <div className="persTitle">Personal Information</div>
+                <div className="inputsPersonal">
+                    <input type="text" className="inputPers" placeholder="First Name"/>
+                    <input type="text" className="inputPers" placeholder="Last Name"/>
+                    <input type="text" className="inputPers" placeholder="Title"/>
+                    <input type="file" className="inputPers fileInput" accept="img/*" title=" "/>
+                    <input type="text" className="inputPers" placeholder="Phone Number"/>
+                    <input type="text" className="inputPers" placeholder="Email Adress"/>
+                    <input type="text" className="inputPers" placeholder="Address"/>
+                    <input type="text" className="inputPers" placeholder="Skills"/>
+                    <textarea  rows="3" className="inputPers about" placeholder="About" />
+                </div>
             </div>
         )
     }
